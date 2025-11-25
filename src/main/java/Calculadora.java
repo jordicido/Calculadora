bf6b14a7caea28eab5e2c7c9c22e02f8b51de5f1
jordicido <jordi.cidoncha4@gmail.com>
2025-11-25 18:20:28 +0100

    operaciones implementadas

--- src/main/java/Calculadora.java
+++ src/main/java/Calculadora.java
@@ -1,5 +1,18 @@
 public class Calculadora {
     public int sumar(int num1, int num2) {
-        return num1+num2+1;
+        return num1+num2;
+    }
+
+    public int restar(int num1, int num2) {
+        return num1-num2;
+    }
+
+    public double dividir(double num1, double num2) {
+        if (num2 == 0) throw new ArithmeticException("No se puede dividir por zero");
+        return num1/num2;
+    }
+
+    public int multiplicar(int num1, int num2) {
+        return num1*num2;
     }
 }
--- src/test/java/CalculadoraTest.java
+++ src/test/java/CalculadoraTest.java
@@ -8,6 +8,26 @@ class CalculadoraTest {
 
     @Test
     public void pruebaSuma() {
-        assertEquals(5,calculadora.sumar(3,2));
+        assertEquals(5, calculadora.sumar(3,2));
+    }
+
+    @Test
+    public void pruebaResta() {
+        assertEquals(13, calculadora.restar(23,10));
+    }
+
+    @Test
+    public void divisionEntreZero() {
+        assertThrows(ArithmeticException.class, () -> calculadora.dividir(13.0, 0.0));
+    }
+
+    @Test
+    public void pruebaDivision() {
+        assertEquals(1.0, calculadora.dividir(13,13));
+    }
+
+    @Test
+    public void pruebaMultiplicacion() {
+        assertEquals(25, calculadora.multiplicar(5, 5));
     }
 }
